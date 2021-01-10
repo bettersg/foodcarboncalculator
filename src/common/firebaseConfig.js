46bdea381871a74d168f431c9e800727350c3477
ryhuz <71387583+ryhuz@users.noreply.github.com>
2021-01-11 00:09:17 +0800

    update firebase.js to include env vars

--- src/common/firebaseConfig.js
+++ src/common/firebaseConfig.js
@@ -3,6 +3,27 @@ import "firebase/auth"
 
 /* Variables */
 // production version to have environment variables
+/*
+export default firebaseApp = firebase.initializeApp {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: "G-MEASUREMENT_ID", // For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
+};
+*/
+
+/* ENV Variables */
+//   REACT_APP_FIREBASE_API_KEY=
+//   REACT_APP_FIREBASE_AUTH_DOMAIN=
+//   REACT_APP_FIREBASE_DATABASE_URL=
+//   REACT_APP_FIREBASE_PROJECT_ID=
+//   REACT_APP_FIREBASE_STORAGE_BUCKET=
+//   REACT_APP_FIREBASE_MESSAGING_SENDER_ID=
+//   REACT_APP_FIREBASE_APP_ID=
 
 export default firebaseApp = firebase.initializeApp {
     apiKey: "API_KEY",
