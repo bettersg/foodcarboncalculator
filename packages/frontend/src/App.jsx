6fcf03bce00c891c72e97a3330fb6a40255dfd54
ryhuz <shawn.wee90@hotmail.com>
2021-01-12 12:29:10 +0800

    added private route for pages with log in required

--- packages/frontend/src/App.jsx
+++ packages/frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import Dashboard from './components/dashboard/Dashboard'
+import { AuthProvider } from './contexts/AuthContext'
 
 import Home from './components/Home'
 import AddMeal from './components/meals/AddMeal'
 import Login from './components/user/Login'
 import Register from './components/user/Register'
-import { AuthProvider } from './contexts/AuthContext'
+import Dashboard from './components/dashboard/Dashboard'
+import PrivateRoute from './components/PrivateRoute'
 
 function App() {
     return (
@@ -18,6 +19,8 @@ function App() {
                     <Route path="/register" component={Register} />
                     <Route path="/dashboard" component={Dashboard} />
                     <Route path="/addmeal" component={AddMeal} />
+
+                    {/* <PrivateRoute path="xxxxxxx" component={xxxxxxx}/> */}
                 </AuthProvider>
             </Switch>
         </BrowserRouter>
