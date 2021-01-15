42daff3cb19f62fbb02dc352b049168608cf40d7
ryhuz <shawn.wee90@hotmail.com>
2021-01-15 22:41:09 +0800

    cleaned up files

--- packages/frontend/src/common/firestoreConfig.js
+++ packages/frontend/src/common/firestoreConfig.js
@@ -1,10 +1,10 @@
-const admin = require("firebase-admin");
+// const admin = require("firebase-admin");
 
-/* consider putting in env */
-const serviceAccount = require("../better-food-carbon-calculator-firebase-adminsdk-uw704-ff0e4809f0.json");
+// /* consider putting in env */
+// const serviceAccount = require("../better-food-carbon-calculator-firebase-adminsdk-uw704-ff0e4809f0.json");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount)
+// });
 
-const db = admin.firestore();
+// const db = admin.firestore();
