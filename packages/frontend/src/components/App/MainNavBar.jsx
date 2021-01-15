86b6d825e54d7f2f47a6f855d98bac11928e857b
ryhuz <shawn.wee90@hotmail.com>
2021-01-15 17:46:35 +0800

    text added to logo

--- packages/frontend/src/components/App/MainNavBar.jsx
+++ packages/frontend/src/components/App/MainNavBar.jsx
@@ -8,7 +8,23 @@ import { ReactComponent as MoreLogo } from '../../static/more_horizontal.svg';
 import { ReactComponent as AddCircleLogo } from '../../static/Ellipse 15.svg';
 import { ReactComponent as PlusInCircle } from '../../static/close_big.svg';
 
-function AddCircle() {
+const HomeButton = () => {
+    return (
+        <div>
+            <div className="button-logo"><HomeLogo /></div>
+            <div>Home</div>
+        </div>
+    )
+}
+const DiaryButton = () => {
+    return (
+        <div>
+            <div className="button-logo"><DiaryLogo /></div>
+            <div>Diary</div>
+        </div>
+    )
+}
+const AddButton = () => {
     return (
         <span id="add-button">
             <span id=""><AddCircleLogo /></span>
@@ -16,16 +32,37 @@ function AddCircle() {
         </span>
     )
 }
+const DatabaseButton = () => {
+    return (
+        <div>
+            <div className="button-logo"><DatabaseLogo /></div>
+            <div>Database</div>
+        </div>
+    )
+}
+const MoreButton = () => {
+    return (
+        <div>
+            <div className="button-logo"><MoreLogo /></div>
+            <div>More</div>
+        </div>
+    )
+}
 
 function MainNavBar() {
     return (
-        <div id="main-nav-bar">
-            <HomeLogo />
-            <DiaryLogo />
-            {AddCircle()}
-            <DatabaseLogo />
-            <MoreLogo />
-        </div>
+        <>
+            <div id="main-nav-bar">
+                <div id="add-items">TESTES</div>
+                <div id="main-nav-container">
+                    {HomeButton()}
+                    {DiaryButton()}
+                    {AddButton()}
+                    {DatabaseButton()}
+                    {MoreButton()}
+                </div>
+            </div>
+        </>
     )
 }
 
