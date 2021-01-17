0c77d07c33d9c509bd08ec194b017c6e21827ed6
TanSK <tansk@Tans-MacBook-Pro.local>
2021-01-17 23:54:56 +0800

    Update CSS stylesheet into a CSS modules stylesheet

--- packages/frontend/src/components/Header/Header.jsx
+++ packages/frontend/src/components/Header/Header.jsx
@@ -2,24 +2,27 @@ import { useState } from 'react';
 import { ReactComponent as LeafLogo } from '../../assets/svg/leaf_logo.svg';
 import { ReactComponent as HamburgerIcon } from '../../assets/svg/hamburger.svg';
 import { ReactComponent as CloseMenuIcon } from '../../assets/svg/close_small.svg';
-import '../../styles/header.css';
+import styles from '../../styles/Header.module.css';
 import { menuItems } from './Mocks';
 
 const DropDownMenu = ({ menuItems, active, setActive }) => {
   return (
-    <div id="header-dropdown-menu" className={`header-dropdown-menu ${active ? 'active' : ''}`}>
+    <div
+      id="header-dropdown-menu"
+      className={`${styles.headerDropdownMenu} ${active ? styles.active : ''}`}
+    >
       <div
         tabIndex="0"
         role="button"
         id="close-menu-icon"
-        className="close-menu-icon"
+        className={styles.closeMenuIcon}
         onClick={() => setActive(!active)}
         onKeyDown={() => setActive(!active)}
       >
         <CloseMenuIcon />
       </div>
       {menuItems.map((item) => (
-        <div role="menuitem" className={`header-dropdown-menu-item`} key={item}>
+        <div role="menuitem" className={styles.headerDropdownMenuItem} key={item}>
           {item}
         </div>
       ))}
@@ -31,17 +34,17 @@ export const Header = () => {
   const [active, setActive] = useState(false);
   return (
     <>
-      <div className="header">
+      <div className={styles.header}>
         <DropDownMenu menuItems={menuItems} active={active} setActive={setActive} />
-        <div className="header-icons">
-          <span id="leaf-logo" className="leaf-logo">
+        <div className={styles.headerIcons}>
+          <span id="leaf-logo" className={styles.leafLogo}>
             <LeafLogo />
           </span>
           <span
             id="hamburger-icon"
             tabIndex="0"
             role="menu"
-            className="hamburger-icon"
+            className={styles.hamburgerIcon}
             onClick={() => setActive(!active)}
             onKeyDown={() => setActive(!active)}
           >
