9e603a9722f62cb9e249b87cc55d956fb4247f6f
Tan <Print-ABC@users.noreply.github.com>
2021-01-18 00:09:38 +0800

    Add header component (#9)
    * Add header component
    
    * Add stylelint-disable to header.css
    
    * Update stylelint-disable
    
    * Update CSS stylesheet into a CSS modules stylesheet
    
    * Remove mocks file
    
    * Remove stylelint-disable and update styles
    
    * fixed
    
    Co-authored-by: TanSK <tansk@Tans-MacBook-Pro.local>
    Co-authored-by: Chan Wen Tjun <wentjun289@hotmail.com>

--- packages/frontend/src/components/Header/Header.jsx
+++ packages/frontend/src/components/Header/Header.jsx
@@ -1,4 +1,58 @@
-// todo: @Tan Suan Khai
+import { useState } from 'react';
+import { ReactComponent as LeafLogo } from '../../assets/svg/leaf_logo.svg';
+import { ReactComponent as HamburgerIcon } from '../../assets/svg/hamburger.svg';
+import { ReactComponent as CloseMenuIcon } from '../../assets/svg/close_small.svg';
+import styles from '../../styles/Header.module.css';
+
+const menuItems = ['Database', 'Diary', 'Log Out', 'Notifcations', 'My account'];
+
+const DropDownMenu = ({ menuItems, active, setActive }) => {
+  return (
+    <div
+      id="header-dropdown-menu"
+      className={`${styles.headerDropdownMenu} ${active ? styles.active : ''}`}
+    >
+      <div
+        tabIndex="0"
+        role="button"
+        id="close-menu-icon"
+        className={styles.closeMenuIcon}
+        onClick={() => setActive(!active)}
+        onKeyDown={() => setActive(!active)}
+      >
+        <CloseMenuIcon />
+      </div>
+      {menuItems.map((item) => (
+        <div role="menuitem" className={styles.headerDropdownMenuItem} key={item}>
+          {item}
+        </div>
+      ))}
+    </div>
+  );
+};
+
 export const Header = () => {
-  return <></>;
+  const [active, setActive] = useState(false);
+  return (
+    <>
+      <div className={styles.header}>
+        <DropDownMenu menuItems={menuItems} active={active} setActive={setActive} />
+        <div className={styles.headerIcons}>
+          <span id="leaf-logo" className={styles.leafLogo}>
+            <LeafLogo />
+          </span>
+          <span
+            id="hamburger-icon"
+            tabIndex="0"
+            role="menu"
+            className={styles.hamburgerIcon}
+            onClick={() => setActive(!active)}
+            onKeyDown={() => setActive(!active)}
+          >
+            <HamburgerIcon />
+          </span>
+        </div>
+      </div>
+    </>
+  );
 };
