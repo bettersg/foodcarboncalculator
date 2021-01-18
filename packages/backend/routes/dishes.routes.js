6647177d668be63d9141c9a20d0a445f42c38840
ryhuz <shawn.wee90@hotmail.com>
2021-01-19 03:26:52 +0800

    added firestore to backend

--- packages/backend/routes/dishes.routes.js
+++ packages/backend/routes/dishes.routes.js
@@ -1,7 +1,18 @@
 const router = require("express").Router();
+const db = require('../config/firebaseConfig');
 
-router.get("/test", (req,res) => {
-    return res.status(200).json({test: 'Dishes test successful!'});
+router.get("/test", async (req, res) => {
+    try {
+        // console.log('here');
+        const testDishes = await db.collection('ingredients').get()
+        
+        let dishes = testDishes.docs.map(doc => doc.data());
+
+        return res.status(200).json({ test: 'Dishes test successful!', dishes });
+    } catch (e) {
+        console.log(e);
+    }
+
 });
 
 module.exports = router;
