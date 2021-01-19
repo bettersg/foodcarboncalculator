7ace1613d13a13ee4807b0bd0606b512ff0ecdd6
ryhuz <shawn.wee90@hotmail.com>
2021-01-19 12:08:11 +0800

    added API endpoint - v1/Search

--- packages/backend/routes/dishes.routes.js
+++ packages/backend/routes/dishes.routes.js
@@ -3,16 +3,35 @@ const db = require('../config/firebaseConfig');
 
 router.get("/test", async (req, res) => {
     try {
-        // console.log('here');
-        const testDishes = await db.collection('ingredients').get()
-        
-        let dishes = testDishes.docs.map(doc => doc.data());
-
         return res.status(200).json({ test: 'Dishes test successful!', dishes });
     } catch (e) {
         console.log(e);
     }
-
 });
+
+router.get("/", async (req, res) => {
+    try {
+        let { keyword } = req.query;
+        keyword = keyword.toLowerCase();
+
+        /* get all dishes */
+        let dishes = await db.collection('dishes').get();
+        let allDishes = []
+
+        /* go through each doc */
+        dishes.forEach(doc => {
+            let newDoc = doc.data();
+            // TODO : MIGHT NEED TO CHECK IF newDoc.createdBy MATCHES CURRENT USER
+            // TO RETURN ONLY THE DISHES THEY ADDED
+            if (newDoc.name.toLowerCase().includes(keyword)) {
+                newDoc.id = doc.id;
+                allDishes.push(newDoc)
+            }
+        });
+        return res.status(200).json({ allDishes })
+    } catch (e) {
+        console.log(e);
+    }
+})
 
 module.exports = router;
