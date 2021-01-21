fed2addc70d61d4dc8e5b416c7a46610dfa23417
ryhuz <shawn.wee90@hotmail.com>
2021-01-21 19:22:24 +0800

    created v1/getDiaryDay

--- packages/backend/routes/diary.routes.js
+++ packages/backend/routes/diary.routes.js
@@ -76,7 +76,7 @@ router.get("/week", async (req, res) => {
                 /* Update final amount */
                 calculateTotals(consumption, thisIngredient, i.weight);
 
-                // consumption.byNutrition
+                // consumption.byCategory
                 consumption.byCategory[category] += i.weight;
             }
         }
@@ -88,74 +88,64 @@ router.get("/week", async (req, res) => {
 })
 
 /**
- * @api {get} /diary/week?user=<userUID>&date=<date> Get carbon data for a given week
- * @apiName v1/getDiaryWeek
+ * @api {get} /diary/day?user=<userUID>&date=<date> Get carbon data for a given day
+ * @apiName v1/getDiaryDay
  * @apiGroup Diary
  *
  * @apiParam {String} <userUID> Search this user's diary
  * @apiParam {String} <date> OPTIONAL: Return entries in the week of this day
  * @apiExample {js} Example usage:
- *      endpoint: /api/v1/diary/week?user=<userUID>&date=<date>
+ *      endpoint: /api/v1/diary/day?user=<userUID>&date=<date>
  * 
  * @apiSuccess (200) {Number} "totalCalories" total kcal in the week
  * @apiSuccess (200) {Number} "totalFootprint" total kg CO2 in the week
  * @apiSuccess (200) {Number} "byNutrition.totalCarbs" g of carbs for the week
  * @apiSuccess (200) {Number} "byNutrition.totalProtein" g of protein for the week
  * @apiSuccess (200) {Number} "byNutrition.totalFat" g of fat for the week
- * @apiSuccess (200) {Number} "byCategory.Whole grain" g of Whole grain for the week
- * @apiSuccess (200) {Number} "byCategory.Vegetables" g of Vegetables for the week
- * @apiSuccess (200) {Number} "byCategory.Fruits" g of Fruits for the week
- * @apiSuccess (200) {Number} "byCategory.Dairy Food" g of Dairy Food for the week
- * @apiSuccess (200) {Number} "byCategory.Protein" g of Protein for the week
- * @apiSuccess (200) {Number} "byCategory.Added fat" g of Added fat for the week
- * @apiSuccess (200) {Number} "byCategory.Added sugar" g of Added sugar for the week
- * @apiSuccess (200) {Number} "byCategory.Tubers or starchy vegetables" g of Tubers or starchy vegetables for the week
  */
-router.get("/week", async (req, res) => {
+router.get("/day", async (req, res) => {
     try {
         let { user, date } = req.query;
         let consumption = {
             totalCalories: 0,
-            totalFootprint: 0,
             byNutrition: {
                 totalCarbs: 0,
                 totalProtein: 0,
                 totalFat: 0,
             },
-            byCategory: {
-                "Whole grain": 0,
-                "Vegetables": 0,
-                "Fruits": 0,
-                "Dairy Food": 0,
-                "Protein": 0,
-                "Added fat": 0,
-                "Added sugar": 0,
-                "Tubers or starchy vegetables": 0
+            meals: {
+                0: [],
+                1: [],
+                2: [],
+                3: [],
             }
+        }
+
+        const calculateTotals = (consumption, thisIngredient, weight) => {
+            consumption.totalCalories += (thisIngredient.calories / 100 * weight);
+            consumption.byNutrition.totalCarbs += thisIngredient.carbs / 100 * weight
+            consumption.byNutrition.totalProtein += thisIngredient.protein / 100 * weight
+            consumption.byNutrition.totalFat += thisIngredient.fat / 100 * weight
+        }
+        const sortMeal = (consumption, dish) => {
+            consumption.meals[dish.data().mealType].push({
+                name: dish.data().dishName,
+                id: dish.id
+            })
         }
 
         /* TODO : FILTER BY DATE */
         let diaryQuery = await db.collection('mealRecords').where("userID", "==", user).get();
 
         for (let entry of diaryQuery.docs) {
+            sortMeal(consumption, entry);
             for (let i of entry.data().ingredients) {
                 /* Retrieve ingredient reference */
                 let queryIngredient = await i.ingredient.get();
                 let thisIngredient = queryIngredient.data();
 
-                /* Retrieve category reference */
-                let queryCategory = await queryIngredient.data().category.get();
-                let category = queryCategory.data().name;
-
                 /* Update final amount */
-                consumption.totalCalories += (thisIngredient.calories / 100 * i.weight);
-                consumption.totalFootprint += (i.weight / 1000 * thisIngredient.footprint);
-                consumption.byNutrition.totalCarbs += thisIngredient.carbs / 100 * i.weight
-                consumption.byNutrition.totalProtein += thisIngredient.protein / 100 * i.weight
-                consumption.byNutrition.totalFat += thisIngredient.fat / 100 * i.weight
-
-                // consumption.byNutrition
-                consumption.byCategory[category] += i.weight;
+                calculateTotals(consumption, thisIngredient, i.weight);
             }
         }
 
