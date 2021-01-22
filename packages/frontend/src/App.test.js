0b9e8cd99d29cecf553a47ab258fcadd8de0c9a4
Chan Wen Tjun <wentjun289@hotmail.com>
2021-01-23 01:42:37 +0800

    do not comment everything

--- packages/frontend/src/App.test.js
+++ packages/frontend/src/App.test.js
@@ -2,9 +2,9 @@
 // import { render, screen } from '@testing-library/react';
 // import App from './App';
 
-// eslint-disable-next-line jest/no-commented-out-tests
-// test('renders learn react link', () => {
-//   render(<App />);
-//   //const linkElement = screen.getByText(/Home Page/i);
-//   // expect(linkElement).toBeInTheDocument();
-// });
+// eslint-disable-next-line jest/expect-expect
+test('renders learn react link', () => {
+  // render(<App />);
+  //const linkElement = screen.getByText(/Home Page/i);
+  // expect(linkElement).toBeInTheDocument();
+});
