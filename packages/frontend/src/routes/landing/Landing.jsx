a53c6114b0f08a9c004d317c32b249d9060700e7
TanSK <tansk@Tans-MacBook-Pro.local>
2021-01-23 16:00:51 +0800

    Add landing page header

--- packages/frontend/src/routes/landing/Landing.jsx
+++ packages/frontend/src/routes/landing/Landing.jsx
@@ -2,6 +2,7 @@
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 import { useAuth } from '../../contexts/AuthContext';
+import { ReactComponent as ClimateDietLogo } from '../../assets/svg/climate_diet_logo.svg';
 import styles from '../../styles/Landing.module.css';
 
 export const Landing = () => {
@@ -17,8 +18,15 @@ export const Landing = () => {
     }
   };
 
+  const LandingPageHeader = styled.div`
+    width: 33px;
+    height: 93px;
+    padding: 34px 0 0;
+  `;
+
   const LandingPage = styled.div`
     color: #ffffff;
+    padding-top: 184px;
     > div {
       width: 100%;
     }
@@ -28,7 +36,7 @@ export const Landing = () => {
     width: fit-content !important;
   `;
 
-  const RegisterButton = styled.div`
+  const Button = styled.div`
     width: 242px;
     height: 57px;
     background: #fac138;
@@ -46,37 +54,42 @@ export const Landing = () => {
 
   console.log(currUser);
   return (
-    <LandingPage>
-      <div className={`${styles.welcomeHeading} ${styles.heading}`}>
-        <div>Welcome to</div>
-        <div className={styles.darkGreenText}>Climate Diet SG</div>
-      </div>
-      <div className={styles.welcomeMsg}>
-        Begin your first step towards sustainable eating here with us!
-      </div>
-      <ButtonWrapper>
-        <NavLink to="/register">
-          <RegisterButton>Register</RegisterButton>
-        </NavLink>
-      </ButtonWrapper>
-      <div className={styles.signIn}>
-        Already have an account? Sign in{' ' /* Please help update lint rules to allow space */}
-        <NavLink className={styles.signInLink} to="/login">
-          here
-        </NavLink>
-      </div>
-      <div>======================================</div>
-      <div>TODO: Remove content below when possible</div>
-      <div>======================================</div>
-      <div>
-        <NavLink to="/dashboard/">Dashboard</NavLink>
-      </div>
+    <>
+      <LandingPageHeader>
+        <ClimateDietLogo />
+      </LandingPageHeader>
+      <LandingPage>
+        <div className={`${styles.welcomeHeading} ${styles.heading}`}>
+          <div>Welcome to</div>
+          <div className={styles.darkGreenText}>Climate Diet SG</div>
+        </div>
+        <div className={styles.welcomeMsg}>
+          Begin your first step towards sustainable eating here with us!
+        </div>
+        <ButtonWrapper>
+          <NavLink to="/register">
+            <Button>Register</Button>
+          </NavLink>
+        </ButtonWrapper>
+        <div className={styles.signIn}>
+          Already have an account? Sign in{' ' /* Please help update lint rules to allow space */}
+          <NavLink className={styles.signInLink} to="/login">
+            here
+          </NavLink>
+        </div>
+        <div>======================================</div>
+        <div>TODO: Remove content below in future</div>
+        <div>======================================</div>
+        <div>
+          <NavLink to="/dashboard/">Dashboard</NavLink>
+        </div>
 
-      <h4>Current User</h4>
-      <div>{currUser ? currUser.email : 'NO ONE LOGGED IN'}</div>
-      <div tabIndex="0" role="button" onClick={handleLogout}>
-        Logout
-      </div>
-    </LandingPage>
+        <h4>Current User</h4>
+        <div>{currUser ? currUser.email : 'NO ONE LOGGED IN'}</div>
+        <div tabIndex="0" role="button" onClick={handleLogout}>
+          Logout
+        </div>
+      </LandingPage>
+    </>
   );
 };
