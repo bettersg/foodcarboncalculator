e984e5f83126a045f84276bc8a4b55c257e7123e
Chan Wen Tjun <wentjun289@hotmail.com>
2021-01-23 21:46:03 +0800

    body component

---
+++ packages/frontend/src/components/layout/Body.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Body = styled.div`
+  background: #f8f8fc;
+  border-radius: 37px 37px 0 0;
+  flex: 1;
+  top: 156px;
+  width: 100%;
+`;
--- packages/frontend/src/routes/create-food/CreateFood.jsx
+++ packages/frontend/src/routes/create-food/CreateFood.jsx
@@ -1,14 +1,22 @@
+import { Body } from '../../components/layout';
 import { NutritionFacts } from '../../components/nutrition-facts';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`;
 
 export const CreateFood = () => {
   return (
-    <div className="page-container">
+    <Wrapper>
       <div className="heading">
         <h1>Create a Food</h1>
       </div>
-      <div>
+      <Body>
         <NutritionFacts calories={475} carbs={61} protein={25} fat={15} />
-      </div>
-    </div>
+      </Body>
+    </Wrapper>
   );
 };
