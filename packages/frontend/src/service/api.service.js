1799fb3c5e26a67527dd8b80ebfa8d9397d56800
Chan Wen Tjun <wentjun289@hotmail.com>
2021-01-24 13:55:47 +0800

    use fetch

--- packages/frontend/src/service/api.service.js
+++ packages/frontend/src/service/api.service.js
@@ -1,5 +1,14 @@
+const BASE_URL = 'http://localhost:3080/api/v1';
+
 export const getIngredients = async () => {
-  const response = await fetch('http://localhost:3080/api/v1/ingredients', {
+  const response = await fetch(`${BASE_URL}/ingredients`, {
+    method: 'GET',
+  });
+  return response.json();
+};
+
+export const getDiaryWeekStatus = async ({ uid }) => {
+  const response = await fetch(`${BASE_URL}/diary/week?user=${uid}`, {
     method: 'GET',
   });
   return response.json();
