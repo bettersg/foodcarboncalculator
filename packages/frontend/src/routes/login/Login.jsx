ea5466caeaa25efada92c6544b529015bfcc84f2
TanSK <tansk@Tans-MacBook-Pro.local>
2021-01-24 16:46:26 +0800

    Move styled components outside of calling component

--- packages/frontend/src/routes/login/Login.jsx
+++ packages/frontend/src/routes/login/Login.jsx
@@ -3,6 +3,89 @@ import { NavLink, Redirect, useHistory } from 'react-router-dom';
 import styled from 'styled-components';
 import { useAuth } from '../../contexts/AuthContext';
 
+const LoginPage = styled.div`
+  width: 100%;
+  color: #fff;
+  padding-top: 184px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const PageWrapper = styled.div`
+  width: 40vw;
+  > div {
+    width: 100%;
+  }
+`;
+
+const PageHeading = styled.div`
+  color: #005a36;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: bold;
+  line-height: 28px;
+  letter-spacing: 0.015em;
+  text-transform: uppercase;
+`;
+
+const BackLinkWrapper = styled.div`
+  width: fit-content;
+  font-size: 16px;
+  font-style: normal;
+  line-height: 28px;
+  > a:hover {
+    text-decoration: underline;
+  }
+`;
+
+const LoginOptions = styled.div`
+  text-transform: capitalize;
+  > div:hover {
+    width: fit-content;
+    cursor: pointer;
+    text-decoration: underline;
+  }
+`;
+
+const Form = styled.form`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 10px 0;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  height: 30px;
+  margin: 0 0 10px;
+`;
+
+const Button = styled.button`
+  width: 100%;
+  height: 40px;
+  background: #fac138;
+  box-sizing: border-box;
+  border: none;
+  border-radius: 29.5px;
+  color: #005a36;
+  font-weight: 600;
+  font-size: 20px;
+  line-height: 125.2%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  outline: none;
+  :active {
+    box-shadow: 0 5px #666;
+    transform: translateY(4px);
+  }
+`;
+
 export const Login = () => {
   const { login, loginWithGoogle, loginWithFacebook, currUser } = useAuth();
   const history = useHistory();
@@ -61,89 +144,6 @@ export const Login = () => {
   if (currUser) {
     return <Redirect to="/dashboard" />;
   }
-
-  const LoginPage = styled.div`
-    width: 100%;
-    color: #fff;
-    padding-top: 184px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-  `;
-
-  const PageWrapper = styled.div`
-    width: 40vw;
-    > div {
-      width: 100%;
-    }
-  `;
-
-  const PageHeading = styled.div`
-    color: #005a36;
-    font-size: 24px;
-    font-style: normal;
-    font-weight: bold;
-    line-height: 28px;
-    letter-spacing: 0.015em;
-    text-transform: uppercase;
-  `;
-
-  const BackLinkWrapper = styled.div`
-    width: fit-content;
-    font-size: 16px;
-    font-style: normal;
-    line-height: 28px;
-    > a:hover {
-      text-decoration: underline;
-    }
-  `;
-
-  const LoginOptions = styled.div`
-    text-transform: capitalize;
-    > div:hover {
-      width: fit-content;
-      cursor: pointer;
-      text-decoration: underline;
-    }
-  `;
-
-  const Form = styled.form`
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    padding: 10px 0;
-  `;
-
-  const Input = styled.input`
-    width: 100%;
-    height: 30px;
-    margin: 0 0 10px;
-  `;
-
-  const Button = styled.button`
-    width: 100%;
-    height: 40px;
-    background: #fac138;
-    box-sizing: border-box;
-    border: none;
-    border-radius: 29.5px;
-    color: #005a36;
-    font-weight: 600;
-    font-size: 20px;
-    line-height: 125.2%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    cursor: pointer;
-    outline: none;
-    :active {
-      box-shadow: 0 5px #666;
-      transform: translateY(4px);
-    }
-  `;
 
   return (
     <LoginPage>
--- packages/frontend/src/routes/register/Register.jsx
+++ packages/frontend/src/routes/register/Register.jsx
@@ -3,6 +3,80 @@ import { NavLink, useHistory } from 'react-router-dom';
 import styled from 'styled-components';
 import { useAuth } from '../../contexts/AuthContext';
 
+const RegisterPage = styled.div`
+  width: 100%;
+  color: #fff;
+  padding-top: 184px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const PageWrapper = styled.div`
+  width: 40vw;
+  > div {
+    width: 100%;
+  }
+`;
+
+const PageHeading = styled.div`
+  color: #005a36;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: bold;
+  line-height: 28px;
+  letter-spacing: 0.015em;
+  text-transform: uppercase;
+`;
+
+const BackLinkWrapper = styled.div`
+  width: fit-content;
+  font-size: 16px;
+  font-style: normal;
+  line-height: 28px;
+  padding-bottom: 20px;
+  > a:hover {
+    text-decoration: underline;
+  }
+`;
+
+const Form = styled.form`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  height: 30px;
+  margin: 0 0 10px;
+`;
+
+const Button = styled.button`
+  width: 100%;
+  height: 40px;
+  background: #fac138;
+  box-sizing: border-box;
+  border: none;
+  border-radius: 29.5px;
+  color: #005a36;
+  font-weight: 600;
+  font-size: 20px;
+  line-height: 125.2%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  outline: none;
+  :active {
+    box-shadow: 0 5px #666;
+    transform: translateY(4px);
+  }
+`;
+
 export const Register = () => {
   const { signup } = useAuth();
   const history = useHistory();
@@ -23,80 +97,6 @@ export const Register = () => {
       }
     }
   };
-
-  const RegisterPage = styled.div`
-    width: 100%;
-    color: #fff;
-    padding-top: 184px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-  `;
-
-  const PageWrapper = styled.div`
-    width: 40vw;
-    > div {
-      width: 100%;
-    }
-  `;
-
-  const PageHeading = styled.div`
-    color: #005a36;
-    font-size: 24px;
-    font-style: normal;
-    font-weight: bold;
-    line-height: 28px;
-    letter-spacing: 0.015em;
-    text-transform: uppercase;
-  `;
-
-  const BackLinkWrapper = styled.div`
-    width: fit-content;
-    font-size: 16px;
-    font-style: normal;
-    line-height: 28px;
-    padding-bottom: 20px;
-    > a:hover {
-      text-decoration: underline;
-    }
-  `;
-
-  const Form = styled.form`
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-  `;
-
-  const Input = styled.input`
-    width: 100%;
-    height: 30px;
-    margin: 0 0 10px;
-  `;
-
-  const Button = styled.button`
-    width: 100%;
-    height: 40px;
-    background: #fac138;
-    box-sizing: border-box;
-    border: none;
-    border-radius: 29.5px;
-    color: #005a36;
-    font-weight: 600;
-    font-size: 20px;
-    line-height: 125.2%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    cursor: pointer;
-    outline: none;
-    :active {
-      box-shadow: 0 5px #666;
-      transform: translateY(4px);
-    }
-  `;
 
   const handleChange = (e) => {
     setForm({
