00f1204e1f37c41da04dce2660abc18addc95001
Tan <Print-ABC@users.noreply.github.com>
2021-01-24 20:15:57 +0800

    Update login and register screens (#31)
    * Update registration page
    
    * Update login page
    
    * Fix lint issues
    
    * Move styled components outside of calling component
    
    * Add InputBar component
    
    Co-authored-by: TanSK <tansk@Tans-MacBook-Pro.local>

--- packages/frontend/src/routes/login/Login.jsx
+++ packages/frontend/src/routes/login/Login.jsx
@@ -1,6 +1,88 @@
 import { useState } from 'react';
 import { NavLink, Redirect, useHistory } from 'react-router-dom';
+import styled from 'styled-components';
+import { InputBar } from '../../components/input-bar';
 import { useAuth } from '../../contexts/AuthContext';
+
+const LoginPage = styled.div`
+  width: 100%;
+  color: #fff;
+  padding-top: 184px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const PageWrapper = styled.div`
+  width: 40vw;
+  > div {
+    width: 100%;
+  }
+`;
+
+const PageHeading = styled.div`
+  color: #005a36;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: bold;
+  line-height: 28px;
+  letter-spacing: 0.015em;
+  text-transform: uppercase;
+`;
+
+const BackLinkWrapper = styled.div`
+  width: fit-content;
+  font-size: 16px;
+  font-style: normal;
+  line-height: 28px;
+  > a:hover {
+    text-decoration: underline;
+  }
+`;
+
+const LoginOptions = styled.div`
+  text-transform: capitalize;
+  > div:hover {
+    width: fit-content;
+    cursor: pointer;
+    text-decoration: underline;
+  }
+`;
+
+const Form = styled.form`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 10px 0;
+  > Input {
+    margin: 0 0 10px;
+  }
+`;
+
+const Button = styled.button`
+  width: 100%;
+  height: 40px;
+  background: #fac138;
+  box-sizing: border-box;
+  border: none;
+  border-radius: 29.5px;
+  color: #005a36;
+  font-weight: 600;
+  font-size: 20px;
+  line-height: 125.2%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  outline: none;
+  :active {
+    box-shadow: 0 5px #666;
+    transform: translateY(4px);
+  }
+`;
 
 export const Login = () => {
   const { login, loginWithGoogle, loginWithFacebook, currUser } = useAuth();
@@ -62,28 +144,32 @@ export const Login = () => {
   }
 
   return (
-    <div>
-      <h1>Login</h1>
-      {existingCredentialError && (
-        <div>
-          You have an account with a different sign-in method. Sign in to your account, then go to
-          settings to link your accounts
-        </div>
-      )}
-      <form onSubmit={handleSubmit}>
-        <input type="text" name="email" placeholder="email" onChange={handleChange} />
-        <input type="text" name="password" placeholder="password" onChange={handleChange} />
-        <button type="submit">submit</button>
-      </form>
-      <div tabIndex="0" role="button" onClick={loginGoogle} onKeyDown={loginGoogle}>
-        click here to login with google
-      </div>
-      <div tabIndex="0" role="button" onClick={loginFaceBook} onKeyDown={loginFaceBook}>
-        click here to login with facebook
-      </div>
-      <div>
-        <NavLink to="/">Back Home</NavLink>
-      </div>
-    </div>
+    <LoginPage>
+      <PageWrapper>
+        <PageHeading>Login</PageHeading>
+        {existingCredentialError && (
+          <div>
+            You have an account with a different sign-in method. Sign in to your account, then go to
+            settings to link your accounts
+          </div>
+        )}
+        <BackLinkWrapper>
+          <NavLink to="/">&lsaquo; Back Home</NavLink>
+        </BackLinkWrapper>
+        <Form onSubmit={handleSubmit}>
+          <InputBar placeholder="email" type="text" changeHandler={handleChange} />
+          <InputBar placeholder="password" type="text" changeHandler={handleChange} />
+          <Button type="submit">submit</Button>
+        </Form>
+        <LoginOptions>
+          <div tabIndex="0" role="button" onClick={loginGoogle} onKeyDown={loginGoogle}>
+            click here to login with google
+          </div>
+          <div tabIndex="0" role="button" onClick={loginFaceBook} onKeyDown={loginFaceBook}>
+            click here to login with facebook
+          </div>
+        </LoginOptions>
+      </PageWrapper>
+    </LoginPage>
   );
 };
--- packages/frontend/src/routes/register/Register.jsx
+++ packages/frontend/src/routes/register/Register.jsx
@@ -1,6 +1,79 @@
 import { useState } from 'react';
 import { NavLink, useHistory } from 'react-router-dom';
+import styled from 'styled-components';
+import { InputBar } from '../../components/input-bar';
 import { useAuth } from '../../contexts/AuthContext';
+
+const RegisterPage = styled.div`
+  width: 100%;
+  color: #fff;
+  padding-top: 184px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const PageWrapper = styled.div`
+  width: 40vw;
+  > div {
+    width: 100%;
+  }
+`;
+
+const PageHeading = styled.div`
+  color: #005a36;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: bold;
+  line-height: 28px;
+  letter-spacing: 0.015em;
+  text-transform: uppercase;
+`;
+
+const BackLinkWrapper = styled.div`
+  width: fit-content;
+  font-size: 16px;
+  font-style: normal;
+  line-height: 28px;
+  padding-bottom: 20px;
+  > a:hover {
+    text-decoration: underline;
+  }
+`;
+
+const Form = styled.form`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  > Input {
+    margin: 0 0 10px;
+  }
+`;
+
+const Button = styled.button`
+  width: 100%;
+  height: 40px;
+  background: #fac138;
+  box-sizing: border-box;
+  border: none;
+  border-radius: 29.5px;
+  color: #005a36;
+  font-weight: 600;
+  font-size: 20px;
+  line-height: 125.2%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  outline: none;
+  :active {
+    box-shadow: 0 5px #666;
+    transform: translateY(4px);
+  }
+`;
 
 export const Register = () => {
   const { signup } = useAuth();
@@ -30,17 +103,19 @@ export const Register = () => {
     });
   };
   return (
-    <div>
-      <h1>Register</h1>
-      {existingCredentialError && <div>This email already exists.</div>}
-      <div>
-        <NavLink to="/">Back Home</NavLink>
-      </div>
-      <form onSubmit={handleSubmit}>
-        <input type="text" name="email" placeholder="email" onChange={handleChange} />
-        <input type="text" name="password" placeholder="password" onChange={handleChange} />
-        <button type="submit">submit</button>
-      </form>
-    </div>
+    <RegisterPage>
+      <PageWrapper>
+        <PageHeading>Register a new account</PageHeading>
+        {existingCredentialError && <div>This email already exists.</div>}
+        <BackLinkWrapper>
+          <NavLink to="/">&lsaquo; Back Home</NavLink>
+        </BackLinkWrapper>
+        <Form onSubmit={handleSubmit}>
+          <InputBar placeholder="email" type="text" changeHandler={handleChange} />
+          <InputBar placeholder="password" type="text" changeHandler={handleChange} />
+          <Button type="submit">submit</Button>
+        </Form>
+      </PageWrapper>
+    </RegisterPage>
   );
 };
